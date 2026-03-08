4e4d3d27d98328fb5f253866751144e30ccb32cf
MARCELO <mcorrea1@byupathway.edu>
2026-03-07 21:02:59 -0300

    exercise 2 finished

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,49 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass. Try harder, and I'm sure you'll make it on your next try.!");
+        }
+
+
     }
 }
