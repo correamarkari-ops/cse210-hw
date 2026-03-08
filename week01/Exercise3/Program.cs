cc5727fbd06c13bda8a6277fa1ee08a6b42cb773
MARCELO <mcorrea1@byupathway.edu>
2026-03-07 22:15:59 -0300

    exercise 3 finished

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,37 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int secretNumber = randomGenerator.Next(1, 101);
+
+         int guess = 0;
+
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+
+      
+       
+        do{
+        
+          
+
+            if (secretNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (secretNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }while (guess == secretNumber);           
     }
 }
